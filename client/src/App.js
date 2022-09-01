a1e0007be3b970a5d783211e7dcbc4a0b3c2c523
Immunomodyluator <venhesterhack@gmail.com>
2022-09-01 17:29:29 +0300

    React hook form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import './App.css'
 
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required()
+  })
+  .required()
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+  const onSubmit = (data) => console.log(data)
   return (
     <div className='App'>
-      <header className='App-header'>gfdgdfgfd</header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName')} />
+        <p>{errors.firstName?.message}</p>
+
+        <input {...register('age')} />
+        <p>{errors.age?.message}</p>
+
+        <input type='submit' />
+      </form>
     </div>
   )
 }
